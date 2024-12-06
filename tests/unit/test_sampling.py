ffebe46aa3fec185ed4220e23417175872a980ed
Michael Platzer <michael.platzer@gmail.com>
2024-12-06 23:09:08 +0100

    fix large ints

---
+++ tests/unit/test_sampling.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+from mostlyai.qa.sampling import pull_data_for_embeddings
+
+
+def test_pull_data_for_embeddings_large_int(tmp_path):
+    df = pd.DataFrame({"cc": [123, 1800218404984585216]}, dtype="Int64")
+    pull_data_for_embeddings(df_tgt=df)
